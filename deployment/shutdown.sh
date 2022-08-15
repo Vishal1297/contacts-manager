5f75b2178fdf870bd727d23af61f4df8f285e746
Vishal Yadav <vishalyadav1746@gmail.com>
2022-08-16 00:43:07 +0530

    fix: deployment setup.

--- deployment/shutdown.sh
+++ deployment/shutdown.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+app-image="ivishalyadav/spring-boot-test"
+kubectl="minikube kubectl --"
+
 # Remove image
 # shellcheck disable=SC2046
-sudo docker rmi $(docker images 'spring-boot-test' -a -q)
+sudo docker rmi $(docker images "${app-image}" -a -q)
 
 sudo docker images
 
-kubectl="minikube kubectl --"
+# Delete app deployment
+${kubectl} delete deployment spring-test-app
 
-# Delete deployment
-$kubectl delete deployment spring-test-app
+# Delete app service
+${kubectl} delete service spring-test-service
 
-# Delete service
-$kubectl delete service spring-test-service
+# Delete mysql deployment
+${kubectl} delete deployment mysql
+
+# Delete mysql service
+${kubectl} delete service mysql
